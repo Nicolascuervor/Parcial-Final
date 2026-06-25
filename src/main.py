0945781a5ea924ab942345aa21a024dfd9b8b81c
Nicolás <ncuervo_175@cue.edu.co>
2026-06-25 17:35:49 -0500

    Fix: Evitar crear tablas en tiempo de importacion

--- src/main.py
+++ src/main.py
@@ -2,8 +2,7 @@ import uvicorn
 from src.database.config import Base, engine
 from src.reservas.api import app
 
-# Crear las tablas en la base de datos al iniciar
-Base.metadata.create_all(bind=engine)
-
 if __name__ == "__main__":
+    # Crear las tablas en la base de datos al iniciar
+    Base.metadata.create_all(bind=engine)
     uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
