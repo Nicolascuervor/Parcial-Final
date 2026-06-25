3b64f648dbae29f5aa6b6f299c1a42bac49dc4e2
Nicolás <ncuervo_175@cue.edu.co>
2026-06-25 18:17:59 -0500

    Fix: Quitar require de cypress en config para entornos sin npm install

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,8 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
     specPattern: "tests/e2e/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+};
